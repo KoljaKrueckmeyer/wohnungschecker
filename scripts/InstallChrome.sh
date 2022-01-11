37fc3eb39e0812eb31f53e51a2f2c528ba795c99
Kolja Krückmeyer <kolja.krueckmeyer@gmx.de>
2022-01-11 22:30:46 +0100

    Update InstallChrome.sh

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 CHROME_VERSION="95.0.4638.54-1"
-wget https://dl.google.com/linux/direct/google-chrome-stable_${CHROME_VERSION}_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+wget --no-verbose -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_${CHROME_VERSION}_amd64.deb
+sudo apt install -y /tmp/chrome.deb
+sudo rm /tmp/chrome.deb
