c3b4ed481eab00ea0465aaff8e1630a1fa5eedfc
Kolja Krückmeyer <kolja.krueckmeyer@gmx.de>
2022-01-11 21:46:56 +0100

    ini

---
+++ src/test/java/com/lazerycode/selenium/page_objects/DhuHomePage.java
@@ -0,0 +1,10 @@
+package com.lazerycode.selenium.page_objects;
+
+import com.lazerycode.selenium.util.Query;
+import org.openqa.selenium.By;
+
+public class DhuHomePage {
+
+    //private final Query searchBar = new Query().defaultLocator(By.);
+
+}
