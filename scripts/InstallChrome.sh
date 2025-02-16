9a9905bc972a1ec3bddabf1e97c43e080f3e8172
w2qmbyq <kmkr@de.ibm.com>
2025-02-16 14:36:42 +0100

    fix gh

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# Install
 sudo apt update
-sudo apt upgrade
-sudo apt install -y wget
-wget -o google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install google-chrome-stable
