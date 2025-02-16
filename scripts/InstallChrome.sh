77ca73e6361e1ed12b22a5081471bb2265169bbc
w2qmbyq <kmkr@de.ibm.com>
2025-02-16 14:25:13 +0100

    fix gh

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+sudo apt update
+sudo apt upgrade
 sudo apt install -y wget
-wget -o ./google-chrome-stable_current_amd64.deb https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_133.0.6943.98-1_amd64.deb
-sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+wget -o google-chrome-stable_current_amd64.deb https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_133.0.6943.98-1_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
